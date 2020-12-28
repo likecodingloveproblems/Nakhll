a121f1a66dca78b1b371c815abe42df8096e0703
likecodeingloveproblems <mohammadali.mehdizadeh@outlook.com>
2020-12-28 16:43:19 +0000

    BIG MODIFICATION: create auth app and move all processes about authentication and authorization to it

---
+++ auth/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from auth import views
+
+app_name = 'auth'
+
+urlpatterns = [
+    # login process
+    path('login/', views.Login.as_view(), name='login'),
+    # logout process
+    path('logout/', views.logout_, name='logout'),
+    # registration process
+    path('register-mobile/', views.RegisterMobile.as_view(), name='register-mobile'),
+    path('register-code/', views.RegisterCode.as_view(), name='register-code'),
+    path('register-data/', views.RegisterData.as_view(), name='register-data'),
+    # forget password process
+    path('forget-password-mobile/', views.ForgetPasswordMobile.as_view(), name='forget-password-mobile'),
+    path('forget-password-code/', views.ForgetPasswordCode.as_view(), name='forget-password-code'),
+    path('forget-password-data/', views.ForgetPasswordData.as_view(), name='forget-password-data'),
+]
