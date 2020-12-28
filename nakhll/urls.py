388b90bd444b2c177ac574a668e20d6e606c9e0f
likecodeingloveproblems <mohammadali.mehdizadeh@outlook.com>
2020-12-28 16:56:28 +0000

    clear unused imports, staticfiles urls is in developement mode useful.

--- nakhll/urls.py
+++ nakhll/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url, re_path
-from django.conf.urls.static import static, serve
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# from django.contrib.sitemaps.views import sitemap
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'^oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
@@ -30,7 +26,4 @@ urlpatterns = [
     url(r'^cart/', include('Payment.urls' , namespace='Payment')),
     url(r'^blog/', include('blog.urls' , namespace='blog')),
     url(r'^app/api/', include('restapi.urls' , namespace='restapi')),
-    
 ]
-urlpatterns += static(settings.STATIC_URL,document_root =  settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root =  settings.MEDIA_ROOT)
