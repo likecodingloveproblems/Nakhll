e524430fcb1329349c306c736b64b0cf9e8b1117
likecodeingloveproblems <mohammadali.mehdizadeh@outlook.com>
2021-01-14 08:54:43 +0000

    set environment to split production from dev errors

--- nakhll/settings.py
+++ nakhll/settings.py
@@ -269,10 +269,8 @@ sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DNS"),
     integrations=[DjangoIntegration()],
     traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    environment=os.environ.get("SENTRY_ENVIRONMENT"),
+    send_default_pii=True,
 )
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
