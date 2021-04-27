6c1017b25d15a504a199374e8854697be259e6b9
zeyd-p <zeyd-p@github.com>
2021-04-27 23:44:47 +0430

    add StoreFormm to create store in django forms

--- nakhll_market/forms.py
+++ nakhll_market/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from django.core.validators import validate_email
+from django.core.validators import (
+    validate_email,
+    RegexValidator
+)
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-from .models import Newsletters
+from .models import (
+    Newsletters,
+    Shop,
+    SubMarket
+)
 
 # Login Form
 class Login (forms.Form):
@@ -29,3 +36,143 @@ class CheckEmail (forms.Form):
         if validate_email(email):
             return email
 
+class CustomMultipleModelChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj
+
+
+# Shop Form - Store ModelForm:
+class StoreForm(forms.ModelForm):
+
+    Title = forms.CharField(
+        label="عنوان حجره",
+        widget=forms.TextInput(attrs={
+            "placeholder": "لطفا نام حجره خود را وارد کنید",
+            "class": "form-control",
+            "type": "text",
+            "minlength":"20",
+            "maxlength":"70",
+        })
+    )
+
+    Slug = forms.CharField(
+        label="شناسه حجره",
+        widget=forms.TextInput(attrs={
+            "placeholder": "لطفا لینک حجره خود را به انگلیسی وارد کنید",
+            "class": "form-control",
+            "type": "text",
+        }),
+        validators=[RegexValidator(r"^[a-z0-9\-_]{2,}")]
+    )
+
+    Description = forms.CharField(
+        label="درباره حجره",
+        widget=forms.Textarea(attrs={
+            "placeholder": "لطفا اطلاعات حجره خود را وارد نمایید",
+            "class": "form-control",
+            "type": "text",
+            "rows":"2"
+        })
+    )
+
+    State = forms.CharField(
+        label="استان",
+        widget=forms.TextInput(attrs={
+            "placeholder": "استان",
+            "class": "form-control",
+            "type": "text",
+        })
+    )
+
+    BigCity = forms.CharField(
+        label="شهرستان",
+        widget=forms.TextInput(attrs={
+            "placeholder": "شهرستان",
+            "class": "form-control",
+            "type": "text",
+        })
+    )
+
+    City = forms.CharField(
+        label="شهر",
+        widget=forms.TextInput(attrs={
+            "placeholder": "شهر",
+            "class": "form-control",
+            "type": "text",
+        })
+    )
+
+    # راسته حجره
+    FK_SubMarket = CustomMultipleModelChoiceField(
+        # label="راسته حجره",
+        queryset=SubMarket.objects.all(),
+        # widget=forms.CheckboxSelectMultiple,
+    )
+
+    Bio = forms.CharField(
+        label="معرفی حجره دار",
+        widget=forms.TextInput(attrs={
+            "placeholder": "خلاصه توضیحی در مورد خودتان...",
+            "class": "form-control",
+            "type": "text",
+            "maxlength":"200",
+            "rows":"2",
+        })
+    )  
+
+    # close days 
+    is_sat_closed = forms.BooleanField(
+        label="شنبه",
+        required = False,
+    )
+
+    is_sun_closed = forms.BooleanField(
+        required=False,
+        label="یک‌شنبه",
+    )
+
+    is_mon_closed = forms.BooleanField(
+        required=False,
+        label="دوشنبه",
+    )
+
+    is_tue_closed = forms.BooleanField(
+        required=False,
+        label="سه‌شنبه",
+    )
+
+    is_wed_closed = forms.BooleanField(
+        required=False,
+        label="چهارشنبه",
+    )
+
+    is_thu_closed = forms.BooleanField(
+        required=False,
+        label="پنج‌شنبه",
+    )
+
+    is_fri_closed = forms.BooleanField(
+        required=False,
+        label="جمعه",
+    )
+
+
+    class Meta:
+        model = Shop
+        fields = [
+            "Title",
+            "Slug",
+            "Description",
+            "State",
+            "BigCity",
+            "City",
+            "FK_SubMarket",
+            "Bio",
+            "is_sat_closed",
+            "is_sun_closed",
+            "is_mon_closed",
+            "is_tue_closed",
+            "is_wed_closed",
+            "is_thu_closed",
+            "is_fri_closed",
+        ]
