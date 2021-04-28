9a7d9bb0dc88625f5b2b12aed8d2954927f3c6ed
amirkermani <amirazimpoor@yahoo.com>
2021-04-28 09:33:34 +0430

    add shop manager name as search field to factors list page

--- nakhll_market/profileviews.py
+++ nakhll_market/profileviews.py
@@ -26,7 +26,7 @@ from django.contrib.auth.decorators import user_passes_test
 # unnecessary imports - TODO: remove later
 # from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
 
 from django.contrib.auth.models import User
@@ -3056,13 +3056,13 @@ def RepalyTicketing(request, ticket_id):
 
 # ---------------------- End Ticketin Section ----------------------
 
-class ProfileAlert(LoginRequiredMixin, View):
+class ProfileAlert(LoginRequiredMixin, TemplateView):
     template_name = 'nakhll_market/profile/pages/alert.html'
     redirect_field_name = 'auth:login'
 
     def get_context_data(self, **kwargs):
         request = self.request
-        context = {}
+        context = super().get_context_data(**kwargs)
         this_profile = Profile.objects.get(FK_User=request.user)
         this_inverntory = request.user.WalletManager.Inverntory
         # Get Menu Item
@@ -3070,92 +3070,16 @@ class ProfileAlert(LoginRequiredMixin, View):
         # Get Nav Bar Menu Item
         navbar = Option_Meta.objects.filter(Title='nav_menu_items')
         # --------------------------------------------------------------------
+        # get all new alert
+        alert = Alert.objects.filter(Seen=False).order_by('DateCreate')
+
         context['This_User_Profile'] = this_profile
         context['This_User_Inverntory'] = this_inverntory
         context['Options'] = options
         context['MenuList'] = navbar
-        context['seen_status'] = False
+        context['Alert'] = alert
+
         return context
-
-    def get_users(self, alerts):
-        return  set(
-                    alerts.values_list(
-                            'FK_User__id', 
-                            'FK_User__username', 
-                            'FK_User__first_name', 
-                            'FK_User__last_name'
-                            )
-                    )
-
-
-    def get(self, request, *args, **kwargs):
-        # get all new alert
-        context = self.get_context_data()
-        alerts = Alert.objects.filter(Seen=False).order_by('DateCreate')
-        users = self.get_users(alerts)
-        context['Alert'] = alerts
-        context['users'] = users
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        # Get Status
-        set_checkout = '0'
-        S_Date = None
-        E_Date = None
-        customer = '000'
-
-        StartDate = request.POST.get("date_start", '')
-        EndDate = request.POST.get("date_end", '')
-        Check_Out = request.POST.get("check_out", '')
-        Customer = request.POST.get("customer_name", '')
-        seen_status = request.POST.get('seen_status', '')
-
-        if (((StartDate != '') and (EndDate != '')) or (Check_Out != '') or (Customer != '') or (seen_status != '')):
-            seen_status = bool(seen_status)
-            alerts = Alert.objects.filter(Seen=seen_status).order_by('-DateCreate')
-            if (StartDate != '') and (EndDate != ''):
-                Start = StartDate.split('-')
-                End = EndDate.split('-')
-                JStart = jdatetime.date(int(Start[0]), int(Start[1]), int(Start[2]))
-                JEnd = jdatetime.date(int(End[0]), int(End[1]), int(End[2]))
-                GStart = jdatetime.JalaliToGregorian(JStart.year, JStart.month, JStart.day)
-                GEnd = jdatetime.JalaliToGregorian(JEnd.year, JEnd.month, JEnd.day)
-                Str_GStart = "%d-%d-%d" % (GStart.gyear, GStart.gmonth, GStart.gday)
-                Str_GEnd = "%d-%d-%d" % (GEnd.gyear, GEnd.gmonth, GEnd.gday)
-                alerts = alerts.filter(DateCreate__range=[Str_GStart, Str_GEnd])
-                S_Date = StartDate
-                E_Date = EndDate
-            if Check_Out != '000':
-                alerts = alerts.filter(Part=Check_Out)
-                set_checkout = Check_Out
-            if Customer != '000':
-                alerts = alerts.filter(FK_User=User.objects.get(id=Customer))
-                customer = User.objects.get(id=Customer).username
-            # ----------------------------------------------------------------------
-            # Get User Info
-            this_profile = Profile.objects.get(FK_User=request.user)
-            this_inverntory = request.user.WalletManager.Inverntory
-            # Get Menu Item
-            options = Option_Meta.objects.filter(Title='index_page_menu_items')
-            # Get Nav Bar Menu Item
-            navbar = Option_Meta.objects.filter(Title='nav_menu_items')
-            users = self.get_users(alerts)
-            alertPaginator = Paginator(alerts, 30)
-            page = request.GET.get('page')
-            alerts = alertPaginator.get_page(page)
-            context['Alert'] = alerts
-            context['Start'] = S_Date
-            context['End'] = E_Date
-            context['CheckOut'] = set_checkout
-            context['Customer'] = customer
-            context['seen_status'] =seen_status
-            context['users'] = users
-        else:
-            return redirect('nakhll_market:Alert')
-        return render(request, self.template_name, context)
-
-
 
 
 # Profile Alert
@@ -4353,7 +4277,8 @@ def ShowAllFactorList(request):
             .filter(UserFactor__PaymentStatus=True, UserFactor__Publish=True) \
             .distinct() \
             .order_by('-UserFactor__OrderDate')
-
+        #get all shop
+        shop=Shop.objects.all()
         context = {
             'This_User_Profile': this_profile,
             'This_User_Inverntory': this_inverntory,
@@ -4361,6 +4286,7 @@ def ShowAllFactorList(request):
             'MenuList': navbar,
             'Factors': factors,
             'User': user_factor,
+            'shop':shop,
         }
         return render(request, 'nakhll_market/profile/pages/all_factors.html', context)
     else:
@@ -4748,7 +4674,7 @@ def ManageFactorFilter(request):
             S_Date = None
             E_Date = None
             customer = '000'
-
+            shop='111'
             try:
                 StartDate = request.POST["date_start"]
             except:
@@ -4772,9 +4698,12 @@ def ManageFactorFilter(request):
             try:
                 Customer = request.POST["customer_name"]
             except:
-                Customer = ''
-
-            if (((StartDate != '') and (EndDate != '')) or (Status != '') or (Check_Out != '') or (Customer != '')):
+                Customer = '000'
+            try:
+                shop_id = request.POST["shop_name"]
+            except:
+                shop_id = '111'
+            if (((StartDate != '') and (EndDate != '')) or (Status != '') or (Check_Out != '') or (Customer != '') or (shop_id !='')):
                 factors = Factor.objects.filter(PaymentStatus=True, Publish=True).order_by('-OrderDate')
                 if (StartDate != '') and (EndDate != ''):
                     Start = StartDate.split('-')
@@ -4788,11 +4717,6 @@ def ManageFactorFilter(request):
                     factors = factors.filter(OrderDate__range=[Str_GStart, Str_GEnd])
                     S_Date = StartDate
                     E_Date = EndDate
-                # get user list
-                user_list = []
-                for item in factors:
-                    user_list.append(item.FK_User)
-                user_list = list(dict.fromkeys(user_list))
 
                 if Status != '6':
                     factors = factors.filter(OrderStatus=Status)
@@ -4804,9 +4728,18 @@ def ManageFactorFilter(request):
                     elif Check_Out == '2':
                         factors = factors.filter(Checkout=True)
                         set_checkout = Check_Out
+                if shop_id != '111':
+                    factors = factors.filter(FK_FactorPost__FK_Product__FK_Shop=shop_id)
+
                 if Customer != '000':
                     factors = factors.filter(FK_User=User.objects.get(id=Customer))
                     customer = User.objects.get(id=Customer).username
+                # get user list
+                user_list = []
+                for item in factors:
+                    user_list.append(item.FK_User)
+                user_list = list(dict.fromkeys(user_list))
+
             else:
                 return redirect("nakhll_market:ShowAllFactorList")
             # ----------------------------------------------------------------------
