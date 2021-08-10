003a4ba5788f88cc2a1b06205dc158adbb34a162
Mohsen Hassani <m.hassani.de@gmail.com>
2021-08-10 16:22:11 +0430

    Prenvet logged-in users from getting new session

--- cart/utils.py
+++ cart/utils.py
@@ -8,7 +8,7 @@ def get_user_or_guest(request):
     # TODO: Since the current version of adding product to cart did 
     # not use guid and use sessions instead, I should check for 
     # sessions and get and set them for now, till nextjs full deploy
-    if not guid:
+    if not user and not guid:
         if not request.session or not request.session.session_key:
             request.session.save()
         guid = request.session.session_key
