4ff5219e03f9412c2581b1fe16f99a6435a9cd77
Mohsen Hassani <m.hassani.de@gmail.com>
2021-08-10 17:15:22 +0430

    Create remove_cart_item api in cart module

--- cart/api.py
+++ cart/api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -27,6 +28,8 @@ class UserCartViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
 
     @action(detail=False, methods=['POST', 'GET'], name='Send active cart to accounting')
     def send_to_accounting(self, request):
+        # TODO: editing items in active cart when cart sent to accounting should be denied
+        # TODO: gettings diffrences is not implemented completely
         cart = self.get_object()
         is_differ, old, new = cart.get_diffrences()
         if is_differ:
@@ -67,30 +70,65 @@ class UserCartItemViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['GET'], name='Add item to active cart')
     def add(self, request, pk):
-        user, guid = get_user_or_guest(self.request)
-        cart = CartManager.user_active_cart(user, guid)
+        # TODO: adding item that already in active cart should be validated with older count
         data = {
             'product': pk,
             'count': 1
         } 
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(data=data)
-        if serializer.is_valid():
-            self.perform_create(serializer)
-            return Response(serializer.data)
-        return Response(serializer.errors)
-        
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+    @action(detail=True, methods=['DELETE'], name='Remove item from active cart')
+    def remove(self, request, pk):
+        item = self.get_object()
+
+        if item.count == 1:
+            item.delete()
+        else:
+            item.count = item.count - 1
+            item.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
     def perform_create(self, serializer):
+        ''' Add selected product to user active cart
+
+            Get user active cart, check if product is already in cart or not,
+            if not, create cart_item, else update that cart_item
+
+            Check if item exists or not, if exists, add count of that item to requested
+            count and do the validation over the product, if not procceed vaidation only
+            with requested count. It should create product in case of non-existance or
+            update in in case that is exists
+        '''
         user, guid = get_user_or_guest(self.request)
         product = serializer.validated_data.get('product')
         count = serializer.validated_data.get('count')
+        active_cart = CartManager.user_active_cart(user, guid)
+
+        cart_item = CartItem.objects.filter(product=product, cart=active_cart).first()
+        if cart_item:
+            count = count + cart_item.count
+
         if not ProductManager.is_product_available(product, count):
             raise ValidationError(_('محصول در دسترس نیست و یا به تعداد کافی از این محصول در انبار وجود ندارد'))
-        active_cart = CartManager.user_active_cart(user, guid)
+
+        # Make product jsonify take some time, so it should be after validation
         product_jsonify = ProductManager.jsonify_product(product)
-        serializer.save(cart=active_cart, product_last_known_state=product_jsonify)
+
+        if cart_item:
+            cart_item.count = count
+            cart_item.product_last_known_state = product_jsonify
+            cart_item.save()
+        else:
+            serializer.save(cart=active_cart, product_last_known_state=product_jsonify)
+
 
     def perform_update(self, serializer):
         # TODO: check if permissions are correct
