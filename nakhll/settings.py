6d9dab5ff527c6bd8f4f650bb6d85089e591c88a
Mohsen Hassani <m.hassani.de@gmail.com>
2021-08-15 17:14:25 +0430

    fix: Setting.py cannot read secret_key

--- nakhll/settings.py
+++ nakhll/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-from logistic.models import PostPriceSetting
 import os
 from django.urls.base import reverse_lazy
 import sentry_sdk
