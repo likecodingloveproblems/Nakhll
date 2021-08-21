0be82eed76fa3ec5894dc65889af873956f5d617
likecodeingloveproblems <mohammadali.mehdizadeh@outlook.com>
2021-08-21 11:39:14 +0000

    migrate coupon data to new coupon app

--- coupon/models.py
+++ coupon/models.py
@@ -4,7 +4,7 @@ from django.utils.timezone import make_aware
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from rest_framework.validators import ValidationError
-from nakhll_market.models import Shop, Product
+from nakhll_market.models import Category, Shop, Product
 from coupon.managers import CouponManager, CouponUsageManager
 from coupon.interfaces import CouponValidation
 
@@ -15,19 +15,29 @@ class Coupon(models.Model, CouponValidation):
     class Meta:
         verbose_name = _('کوپن تخفیف')
         verbose_name_plural = _('کوپن‌های تخفیف')
+    AVAILABLE_STATUS =(
+        (True,'فعال'),
+        (False,'غیر فعال'),
+    )
     code = models.CharField(_('کد تخفیف'), max_length=100, unique=True)
-    valid_from = models.DateTimeField(_('تاریخ شروع'), default=datetime.now, null=True, blank=True)
-    valid_to = models.DateTimeField(_('تاریخ پایان'), null=True, blank=True)
-    max_count = models.IntegerField(default=1, verbose_name=_('دفعات استفاده'))
-    is_publish = models.BooleanField(_('وضعیت انتشار'), default=True)
+    valid_from = models.DateField(_('تاریخ شروع'), default=datetime.now, null=True, blank=True)
+    valid_to = models.DateField(_('تاریخ پایان'), null=True, blank=True)
+    user_max_count = models.IntegerField(default=1, verbose_name=_('دفعات استفاده کاربر'))
+    total_max_count = models.IntegerField(default=5, verbose_name=_('دفعات استفاده'))
     price = models.IntegerField(_('مبلغ تخفیف'), default=0)
+    discount_percent = models.IntegerField(_('درصد تخفیف'), default=0)
     min_price = models.IntegerField(_('حداقل خرید'), null=True, blank=True)
     max_price = models.IntegerField(_('حداکثر خرید'), null=True, blank=True)
-    users = models.ManyToManyField(User, verbose_name=_('کاربران'), blank=True, related_name='coupons')
-    shop = models.ForeignKey(Shop, null=True, blank=True, verbose_name=_('حجره'), on_delete=models.SET_NULL, related_name='coupons')
-    product = models.ForeignKey(Product, null=True, blank=True, verbose_name=_('محصول'), on_delete=models.SET_NULL, related_name='coupons')
+    users = models.ManyToManyField(User, verbose_name=_('کاربران'), related_name='coupons')
+    shops = models.ManyToManyField(Shop, verbose_name=_('حجره'), related_name='coupons')
+    products = models.ManyToManyField(Product, verbose_name=_('محصول'), related_name='coupons')
+    categories = models.ManyToManyField(Category, verbose_name=_('دسته بندی'), related_name='coupons')
     description = models.TextField(_('توضیحات'), null=True, blank=True )
-    objects = CouponManager
+    title = models.CharField(_('عنوان'), max_length=100, null=True, blank=True)
+    creator = models.ForeignKey(User, verbose_name=_('سازنده کوپن'), on_delete=models.SET_NULL, null=True, blank=True, related_name='coupon_creator')
+    available=models.BooleanField(verbose_name='وضعیت ثبت کوپن', choices=AVAILABLE_STATUS, default=True)
+    log = models.TextField(_('گزارش'), null=True, blank=True)
+    objects = CouponManager()
 
     def __str__(self):
         return self.code
