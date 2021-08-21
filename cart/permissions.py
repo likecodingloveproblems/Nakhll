04157fc05bda64736413e4aa244669860b643fea
Mohsen Hassani <m.hassani.de@gmail.com>
2021-08-21 12:29:51 +0430

    Fix IsCartItemOwner permission error

--- cart/permissions.py
+++ cart/permissions.py
@@ -1,3 +1,4 @@
+from cart.models import Cart
 from rest_framework.permissions import BasePermission
 from cart.utils import get_user_or_guest
 
@@ -16,9 +17,10 @@ class IsCartItemOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         prev_result = super().has_object_permission(request, view, obj)
         user, guid = get_user_or_guest(request)
+        cart = obj if type(obj) == Cart else obj.cart
         if user:
-            result = obj.cart.user == user
+            result = cart.user == user
         else:
-            result = obj.cart.guest_unique_id == guid
+            result = cart.guest_unique_id == guid
         return prev_result & result
 
