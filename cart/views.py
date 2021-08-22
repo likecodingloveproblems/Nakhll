2123bbdf0f65989ad83778d074ea0f29c13d3d93
Mohsen Hassani <m.hassani.de@gmail.com>
2021-08-22 13:34:25 +0430

    Create add_to_cart view for old landing page

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from cart.api import UserCartItemViewSet
 from datetime import datetime, timedelta
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
@@ -11,53 +12,17 @@ from cart.models import Cart, CartItem
 
 
 def add_to_cart(request, product_ID):
-    ''' Get product and add to user cart 
-        Check if user logged in or not add to session for guest users and add
-        to db if user logged in
+    ''' Get product and add it to cart with cart apis
+   
+        This method is only exists until frontend implement landing page
+        and product details page, then they should handle adding product 
+        to cart and this method can be deleted
     '''
-    # TODO: Check for Product attributes and AttrPrice
-    # TODO: Check for Coupons
-    # // TODO: Check prev cart items and check if shop has enough item in stock
-
-    # Check product availability
-    product = get_object_or_404(Product, ID=product_ID)
-    if (product.Status == '4') or (product.Inventory == 0):
-        return redirect('nakhll_market:Re_ProductsDetail',
-                        shop_slug=product.get_shop_slug(),
-                        product_slug=product.Slug,
-                        status=True,
-                        msg='محصول مدنظر شما در حال حاضر موجود نمی باشد!')
-
-    # Check for user login status
-    # Create cart base on User object or session id
-    if request.user.is_authenticated:
-        cart, created = Cart.objects.get_or_create(user=request.user)
-    else:
-        if not request.session.session_key:
-            request.session._get_or_create_session_key()
-        session, created = Session.objects.get_or_create(
-            session_key=request.session.session_key, defaults={'expire_date': datetime.now() + timedelta(days=5)})
-        cart, created = Cart.objects.get_or_create(session=session)
-
-    # Create cart items
-    cart_item, created = CartItem.objects.get_or_create(cart=cart,
-                                                        product=product, defaults={'count': 1})
-    if not created:
-        # First, check if shop owner has enough items in stock
-        prev_count = cart_item.count
-        if prev_count + 1 > product.Inventory:
-            return redirect('nakhll_market:Re_ProductsDetail',
-                            shop_slug=product.get_shop_slug(),
-                            product_slug=product.Slug,
-                            status=True,
-                            msg='.فروشنده بیش از این تعداد قادر به تامین  کالا نمی باشد')
-        # and now, add one more to cart
-        cart_item.count += 1
-        cart_item.save()
-
-    return redirect('nakhll_market:ProductsDetail',
-                    shop_slug=product.get_shop_slug(),
-                    product_slug=product.Slug)
+    cartviewset = UserCartItemViewSet()
+    request.query_params = {}
+    cartviewset.request = request
+    cartviewset.add(request, product_ID)
+    return redirect('/cart/')
 
 
 def show_cart(request):
