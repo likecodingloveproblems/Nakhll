3f0a9d66c2f527b8ac9397aa45edc5db313a8639
Mohsen Hassani <m.hassani.de@gmail.com>
2021-09-30 14:17:35 +0330

    Create sms interface

--- sms/services.py
+++ sms/services.py
@@ -1,3 +1,5 @@
+import requests
+from nakhll.settings import KAVENEGAR_KEY
 from sms.models import SMS, SMSRequest
 
 # only for test purposes
@@ -6,3 +8,47 @@ def count_sms():
 
 def create_sms(**kwargs):
     SMS.objects.create(**kwargs)
+
+
+def send_sms(Title, Description, PhoneNumber):
+    # Change Text
+    des_list = Description.split(' ')
+    final_des = ''
+    count = 0
+    for item in des_list:
+        if (count + 1) == len(des_list):
+            final_des += item
+            count += 1
+        else:
+            final_des += item + '-'
+            count += 1
+
+    url = 'https://api.kavenegar.com/v1/{}/verify/lookup.json'.format(KAVENEGAR_KEY) 
+    params = {'receptor': PhoneNumber, 'token' : Title, 'token2' : final_des, 'template' : 'nakhll-alert'}
+    requests.post(url, params = params)
+
+
+class Kavenegar:
+    @staticmethod
+    def _raw_send(template, receptor, tokens):
+        url = 'https://api.kavenegar.com/v1/{}/verify/lookup.json'.format(KAVENEGAR_KEY) 
+        params = {'receptor': receptor, 'template' : template, **tokens}
+        requests.post(url, params=params)
+
+    @staticmethod
+    def alert(receptor, title, description):
+        tokens = {
+            'token': title,
+            'token2': description,
+        }
+        KAVENEGAR_KEY._raw_send('nakhll-alert', receptor, tokens)
+
+    @staticmethod
+    def shop_new_order(receptor, order_id):
+        tokens = {
+            'token': order_id,
+        }
+        KAVENEGAR_KEY._raw_send('nakhll-order', receptor, tokens)
+
+
+    
