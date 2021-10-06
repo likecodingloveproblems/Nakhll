79ae114cc68e7d0d36a5513dc8d553df8b541c7e
Mohsen Hassani <m.hassani.de@gmail.com>
2021-10-06 16:07:06 +0330

    Change initiate_response from token to url

--- payoff/payment.py
+++ payoff/payment.py
@@ -81,10 +81,8 @@ class Pec(PaymentMethod):
             raise ValidationError(f'{token_object.Status} {token_object.Message}')
         print(f'\t token: {token_object.Token}')
         print('\n>>>>>>>>>>>>>>>>Redirecting<<<<<<<<<<<<<<<<<<<\n')
-        return Response({'token': token_object.Token, 'ipg_port': Transaction.IPGTypes.PEC})
-        # return token_object.Token
-        # url = f'https://pec.shaparak.ir/NewIPG/?token={token_object.Token}'
-        # return redirect(url)
+        url = f'https://pec.shaparak.ir/NewIPG/?token={token_object.Token}'
+        return Response({'url': url})
 
     def _get_token_object(self):
         ''' Get sale service and send invocie data to it, return token if 
