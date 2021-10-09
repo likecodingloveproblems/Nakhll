0d2d1d266c10daa3ceeb93434c8933c9607eb799
Mohsen Hassani <m.hassani.de@gmail.com>
2021-10-09 11:32:42 +0330

    Add jalali date and time fields in invoice details

--- accounting_new/serializers.py
+++ accounting_new/serializers.py
@@ -1,3 +1,4 @@
+import jdatetime
 from rest_framework import serializers
 from .models import Invoice, InvoiceItem
 from cart.models import Cart
@@ -11,10 +12,21 @@ from nakhll_market.serializers import UserSerializer
 
 class InvoiceItemSerializer(serializers.ModelSerializer):
     product = serializers.PrimaryKeyRelatedField(read_only=True)
+    added_date_jalali = serializers.SerializerMethodField()
+    added_time_jalali = serializers.SerializerMethodField()
     class Meta:
         model = InvoiceItem
         fields = ['id', 'product', 'slug', 'name', 'count', 'price_with_discount', 'price_without_discount', 'weight', 
-                    'image', 'image_thumbnail', 'shop_name', 'added_datetime']
+                    'image', 'image_thumbnail', 'shop_name', 'added_date_jalali', 'added_time_jalali']
+
+    def get_added_date_jalali(self, obj):
+        jalali_datetime = jdatetime.datetime.fromgregorian(datetime=obj.added_datetime, locale='fa_IR')
+        return jalali_datetime.strftime('%Y/%m/%d')
+        
+    def get_added_time_jalali(self, obj):
+        jalali_datetime = jdatetime.datetime.fromgregorian(datetime=obj.added_datetime, locale='fa_IR')
+        return jalali_datetime.strftime('%H:%M')
+        
 
 class InvoiceWriteSerializer(serializers.ModelSerializer):
     address = serializers.PrimaryKeyRelatedField(queryset=Address.objects.all(), required=False)
@@ -34,10 +46,20 @@ class InvoiceRetrieveSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False, read_only=True)
     items = InvoiceItemSerializer(many=True, read_only=True)
     receiver_mobile_number = serializers.SerializerMethodField()
+    created_time_jalali = serializers.SerializerMethodField()
+    created_date_jalali = serializers.SerializerMethodField()
     def get_receiver_mobile_number(self, obj):
         if obj.address:
             return obj.address.receiver_mobile_number
         return None
+
+    def get_created_date_jalali(self, obj):
+        jalali_datetime = jdatetime.datetime.fromgregorian(datetime=obj.created_datetime, locale='fa_IR')
+        return jalali_datetime.strftime('%Y/%m/%d')
+
+    def get_created_time_jalali(self, obj):
+        jalali_datetime = jdatetime.datetime.fromgregorian(datetime=obj.created_datetime, locale='fa_IR')
+        return jalali_datetime.strftime('%H:%M')
     class Meta:
         model = Invoice
         fields = (
@@ -54,7 +76,8 @@ class InvoiceRetrieveSerializer(serializers.ModelSerializer):
             'logistic_price',
             'logistic_errors',
             'final_price',
-            'created_datetime',
+            'created_date_jalali',
+            'created_time_jalali',
             'coupons_total_price',
             'payment_request_datetime',
             'receiver_mobile_number',
