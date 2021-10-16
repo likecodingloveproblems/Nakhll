d3cca04ff453dc9863402280a2b2afd202945eaf
Mohsen Hassani <m.hassani.de@gmail.com>
2021-10-16 10:58:58 +0330

    Add receiver_full_name in invoice list in djagno admin

--- accounting_new/admin.py
+++ accounting_new/admin.py
@@ -9,19 +9,27 @@ admin.site.register(InvoiceItem)
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
     list_display=('id', 'FactorNumber', 'user', 'status',  'final_price', 'post_price',
-                    'mobile_number', 'created_datetime_jalali',  )
+                    'receiver_mobile_number', 'receiver_full_name', 'created_datetime_jalali',  )
     list_filter=('status','user',)
     readonly_fields = ('id',)
     ordering=['-created_datetime', ]
     search_fields = ('id', 'FactorNumber')
 
-    def mobile_number(self, obj):
+    def receiver_mobile_number(self, obj):
         user_mobile_number = obj.user.User_Profile.MobileNumber
         if obj.address_json:
             address = json.loads(obj.address_json)
             return address.get('receiver_mobile_number', user_mobile_number)
         return user_mobile_number
-    mobile_number.short_description = 'شماره همراه'
+    receiver_mobile_number.short_description = 'شماره همراه'
+
+    def receiver_full_name(self, obj):
+        if obj.address_json:
+            address = json.loads(obj.address_json)
+            return address.get('receiver_full_name', '')
+        return ''
+    receiver_full_name.short_description = 'نام گیرنده'
+
 
     def created_datetime_jalali(self, obj):
         return jdatetime.datetime.fromgregorian(datetime=obj.created_datetime).strftime('%Y/%m/%d %H:%M:%S')
