f65f268d1d1ab069b8834f24f77805d9ed76e7cd
Mohsen Hassani <m.hassani.de@gmail.com>
2021-10-25 16:11:47 +0330

    Add undo feature to bulk product operations

--- nakhll_market/product_bulk_operations.py
+++ nakhll_market/product_bulk_operations.py
@@ -2,14 +2,21 @@ import os
 import zipfile
 import random
 import shutil
+from django.db import models
 import pandas as pd
 from django.utils.text import slugify
 from django.conf import settings
-from nakhll_market.models import Product, ProductBanner
+from simple_history.models import HistoricalRecords
+from simple_history.utils import bulk_update_with_history, bulk_create_with_history
+from nakhll_market.models import Product, ProductBanner, HistoricalProduct
 
+
+HISTORY_TYPE_CREATE = '+'
+HISTORY_TYPE_UPDATE = '~'
+HISTORY_TYPE_DELETE = '-'
 
 class BulkProductHandler:
-    def __init__(self, *, shop, images_zip_file=None, barcode_field='barcode',
+    def __init__(self, *, shop, images_zip_file=None, product_barcode_field='barcode',
                  required_fields_with_types={'Title': object, 'barcode': object,
                                 'Price': 'Int64', 'OldPrice': 'Int64', 'Inventory': 'Int64'},
                  optional_fields_with_types={
@@ -18,8 +25,10 @@ class BulkProductHandler:
                  image_fields={'image_1': object, 'image_2': object, 'image_3': object}):
         self.df = None
         self.shop = shop
+        self.shop_key = f'tag:{shop.ID}'
+        self.old_shop_key = f'old_tag:{shop.ID}'
         self.na_rows = None
-        self.barcode_field = barcode_field
+        self.product_barcode_field = product_barcode_field
         self.images_path = self.parse_images_zip(images_zip_file)
         self.slug_duplicate_rows = None
         self.required_fields_with_types = required_fields_with_types
@@ -43,6 +52,7 @@ class BulkProductHandler:
         return result
 
     def save_to_db(self, df):
+        self.__delete_previous_product_history()
         new_products_df, old_products_df = self.__seperate_new_old_products(df)
         old_products = self.__create_old_products_instance(old_products_df)
         new_products = self.__create_new_products_instance(new_products_df)
@@ -69,23 +79,29 @@ class BulkProductHandler:
 
     def __create_old_products_instance(self, df):
         products = Product.objects.filter(
-            FK_Shop=self.shop, barcode__in=df[self.barcode_field].to_list())
+            FK_Shop=self.shop, barcode__in=df[self.product_barcode_field].to_list())
         update_list = []
         for product in products:
             for field in self.update_fields:
                 if field in df.columns:
                     setattr(
                         product, field, df[field][
-                                df[self.barcode_field] == product.barcode].values[0])
+                                df[self.product_barcode_field] == product.barcode].values[0])
             update_list.append(product)
-        Product.objects.bulk_update(update_list, self.update_fields)
+        bulk_update_with_history(update_list, Product, self.update_fields, batch_size=500,
+                                    default_user=self.shop.FK_ShopManager,
+                                    default_change_reason=f'tag:{self.shop.ID}')
+        # Product.objects.bulk_update(update_list, self.update_fields)
         return update_list
 
     def __create_new_products_instance(self, df):
         product_images_dictioanry = self.__pop_images_dictioanry(df)
         new_products = [Product(FK_Shop=self.shop, **row)
                         for row in df.T.to_dict().values()]
-        Product.objects.bulk_create(new_products)
+        objs = bulk_create_with_history(new_products, Product, batch_size=500, 
+                                        default_user=self.shop.FK_ShopManager,
+                                        default_change_reason=f'tag:{self.shop.ID}')
+        # Product.objects.bulk_create(new_products)
         self.__append_images_to_products(df, product_images_dictioanry)
         return new_products
 
@@ -94,7 +110,7 @@ class BulkProductHandler:
         bulk_product_banner_list = []
         new_products_sorted = Product.objects.filter(
                             FK_Shop=self.shop, barcode__in=new_products_df[
-                            self.barcode_field].to_list()).order_by(self.barcode_field)
+                            self.product_barcode_field].to_list()).order_by(self.product_barcode_field)
         for product, product_image_item in zip(new_products_sorted, product_images_dictioanry):
             images = product_image_item[1]
             if images:
@@ -142,19 +158,31 @@ class BulkProductHandler:
         self.slug_duplicate_rows = df[~df.index.isin(duplicate_free.index)]
         return duplicate_free
 
+    def __delete_previous_product_history(self):
+        old_shop_key = f'old_tag:{self.shop.ID}'
+        Product.history.filter(history_change_reason=old_shop_key).delete()
+        product_histories = Product.history.filter(history_change_reason=self.shop_key)
+        bulk_update_list = []
+        for product_history in product_histories:
+            product_history.history_change_reason = old_shop_key
+            bulk_update_list.append(product_history)
+        HistoricalProduct.objects.bulk_update(bulk_update_list, ['history_change_reason'])
+            
+
+
     def __seperate_new_old_products(self, df):
         new_products = df.query(
-            f'{self.barcode_field} not in @Product.objects.filter(FK_Shop=@self.shop)\
-                                    .values_list(@self.barcode_field, flat=True)')
+            f'{self.product_barcode_field} not in @Product.objects.filter(FK_Shop=@self.shop)\
+                                    .values_list(@self.product_barcode_field, flat=True)')
         old_products = df.query(
-            f'{self.barcode_field} in @Product.objects.filter(FK_Shop=@self.shop).\
-                                    values_list(@self.barcode_field, flat=True)')
+            f'{self.product_barcode_field} in @Product.objects.filter(FK_Shop=@self.shop).\
+                                    values_list(@self.product_barcode_field, flat=True)')
         return new_products, old_products
 
     def __pop_images_dictioanry(self, df):
         images_dictioanry = {}
         for index, row in df.iterrows():
-            barcode = row[self.barcode_field]
+            barcode = row[self.product_barcode_field]
             images = []
             for image_field in self.image_fields:
                 image_path = row.get(image_field)
@@ -171,3 +199,39 @@ class BulkProductHandler:
 
     def __delete_image_files(self):
         shutil.rmtree(self.images_path)
+
+
+    def undo_last_changes(self):
+        self.__delete_newly_created_products()
+        self.__revert_updated_products()
+        self.__revert_undo_shop_tag()
+
+    def __delete_newly_created_products(self):
+        newly_added_historical = Product.history.filter(
+                                    history_change_reason=self.shop_key,
+                                    history_type=HISTORY_TYPE_CREATE)
+        newly_added_historical_ids = newly_added_historical.values_list('ID', flat=True)
+        newly_added_products = Product.objects.filter(ID__in=newly_added_historical_ids)
+        newly_added_products.delete()
+        newly_added_historical.delete()
+
+    def __revert_updated_products(self):
+        updated_historical_products = Product.history.filter(history_change_reason=self.shop_key, history_type=HISTORY_TYPE_UPDATE)
+        bulk_update_list = []
+        for hist in updated_historical_products:
+            product = hist.instance
+            if hist.prev_record:
+                for field in self.update_fields:
+                    setattr(product, field, getattr(hist.prev_record.instance, field))
+                bulk_update_list.append(product)
+        Product.objects.bulk_update(bulk_update_list, self.update_fields)
+
+    def __revert_undo_shop_tag(self):
+        old_product_histories = Product.history.filter(history_change_reason=self.old_shop_key)
+        bulk_update_list = []
+        for product_history in old_product_histories:
+            product_history.history_change_reason = self.shop_key
+            bulk_update_list.append(product_history)
+        HistoricalProduct.objects.bulk_update(bulk_update_list, ['history_change_reason'])
+         
+
