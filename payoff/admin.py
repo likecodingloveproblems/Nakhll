2f7c595d2d24e662bf15528bb2074b243f73c893
Mohsen Hassani <m.hassani.de@gmail.com>
2021-12-07 11:46:47 +0330

    Add transaction details in django admin panel

--- payoff/admin.py
+++ payoff/admin.py
@@ -34,3 +34,48 @@ class TransactionAdmin(admin.ModelAdmin):
         return datetime.strftime('%Y/%m/%d %H:%M')
     payoff_datetime_jalali.short_description = 'زمان پایان'
 
+
+
+@admin.register(TransactionResult)
+class TransactionResultAdmin(admin.ModelAdmin):
+    list_display=('id', 'token', 'order_id', 'status', 'amount',
+                  'created_datetime_jalali', 'trans_id' )
+    list_filter=('status', 'created_datetime', 'amount')
+    ordering=['-id', ]
+    fields=('id', 'token', 'order_id', 'terminal_no', 'rrn', 'status', 'amount',
+            'hash_card_number', 'created_datetime_jalali', 'discounted_amount', )
+    readonly_fields = fields + ('trans_id', )
+
+    def created_datetime_jalali(self, obj):
+        datetime = localtime(obj.created_datetime)
+        datetime = jdatetime.datetime.fromgregorian(datetime=datetime)
+        return datetime.strftime('%Y/%m/%d %H:%M')
+    created_datetime_jalali.short_description = 'زمان شروع'
+
+    def trans_id(self, obj):
+        return obj.transaction.id if hasattr(obj, 'transaction') and obj.transaction else ''
+    trans_id.short_description = 'شماره تراکنش'
+
+@admin.register(TransactionConfirmation)
+class TransactionConfirmationAdmin(admin.ModelAdmin):
+    list_display=('id', 'token', 'rrn', 'status', 'trans_res_id',
+                  'created_datetime_jalali')
+    list_filter=('status', 'created_datetime', 'amount')
+    ordering=['-id', ]
+    fields=('id', 'token', 'rrn', 'status', 'transaction_result',
+            'card_number_masked', 'created_datetime_jalali',)
+    readonly_fields = fields + ('trans_res_id', )
+
+    def created_datetime_jalali(self, obj):
+        datetime = localtime(obj.created_datetime)
+        datetime = jdatetime.datetime.fromgregorian(datetime=datetime)
+        return datetime.strftime('%Y/%m/%d %H:%M')
+    created_datetime_jalali.short_description = 'زمان شروع'
+
+    def trans_res_id(self, obj):
+        return (
+                obj.transaction_result.id
+                if hasattr(obj, 'transaction_result') and obj.transaction_result
+                else ''
+            )
+    trans_res_id.short_description = 'شماره تراکنش'
