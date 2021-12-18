6bf54ff02f29002d7c91e8a2aeb8bc500d0dafa5
Mohsen Hassani <m.hassani.de@gmail.com>
2021-12-18 12:09:31 +0330

    Change some serializers

--- logistic/models.py
+++ logistic/models.py
@@ -15,19 +15,28 @@ class Address(models.Model):
         verbose_name = _('آدرس')
         verbose_name_plural = _('آدرس‌ها')
     old_id = models.UUIDField(null=True, blank=True)
-    user = models.ForeignKey(User, verbose_name=_('کاربر'), related_name='addresses', on_delete=models.CASCADE)
-    state = models.ForeignKey(State, verbose_name=_('استان'), related_name='addresses', on_delete=models.CASCADE)
-    big_city = models.ForeignKey(BigCity, verbose_name=_('شهرستان'), related_name='addresses', on_delete=models.CASCADE)
-    city = models.ForeignKey(City, verbose_name=_('شهر'), related_name='addresses', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name=_(
+        'کاربر'), related_name='addresses', on_delete=models.CASCADE)
+    state = models.ForeignKey(State, verbose_name=_(
+        'استان'), related_name='addresses', on_delete=models.CASCADE)
+    big_city = models.ForeignKey(BigCity, verbose_name=_(
+        'شهرستان'), related_name='addresses', on_delete=models.CASCADE)
+    city = models.ForeignKey(City, verbose_name=_(
+        'شهر'), related_name='addresses', on_delete=models.CASCADE)
     address = models.TextField(verbose_name=_('آدرس'))
     zip_code = models.CharField(verbose_name=_('کد پستی'), max_length=10)
-    phone_number = models.CharField(verbose_name=_('تلفن ثابت'), max_length=11, null=True, blank=True)
-    receiver_full_name = models.CharField(verbose_name=_('نام و نام خانوادگی گیرنده'), max_length=200)
-    receiver_mobile_number = models.CharField(verbose_name=_('تلفن همراه گیرنده'), max_length=11)
+    phone_number = models.CharField(verbose_name=_(
+        'تلفن ثابت'), max_length=11, null=True, blank=True)
+    receiver_full_name = models.CharField(verbose_name=_(
+        'نام و نام خانوادگی گیرنده'), max_length=200)
+    receiver_mobile_number = models.CharField(
+        verbose_name=_('تلفن همراه گیرنده'), max_length=11)
 
     objects = AddressManager()
+
     def __str__(self):
         return f'{self.user}: {self.address}'
+
     def to_json(self):
         address_data = {
             'state': self.state.name,
@@ -47,25 +56,35 @@ class PostPriceSetting(models.Model, PostPriceSettingInterface):
     class Meta:
         verbose_name = _('تنظیمات قیمت پستی')
         verbose_name_plural = _('تنظیمات قیمت پستی')
-    user = models.ForeignKey(User, verbose_name=_('کاربر'), related_name='post_price_settings', on_delete=models.SET_NULL, null=True)
-    inside_city_price = models.PositiveIntegerField(verbose_name=_('قیمت پست درون شهری (ریال)'), default=150000)
-    outside_city_price = models.PositiveIntegerField(verbose_name=_('قیمت پست برون شهری (ریال)'), default=200000)
-    extra_weight_fee = models.PositiveIntegerField(verbose_name=_('قیمت به ازای هر کیلو اضافه (ریال)'), default=20000)
-    created_datetime = models.DateTimeField(verbose_name=_('تاریخ ایجاد'), auto_now_add=True)
-    updated_datetime = models.DateTimeField(verbose_name=_('تاریخ بروزرسانی'), auto_now=True)
+    user = models.ForeignKey(User, verbose_name=_(
+        'کاربر'), related_name='post_price_settings', on_delete=models.SET_NULL, null=True)
+    inside_city_price = models.PositiveIntegerField(
+        verbose_name=_('قیمت پست درون شهری (ریال)'), default=150000)
+    outside_city_price = models.PositiveIntegerField(
+        verbose_name=_('قیمت پست برون شهری (ریال)'), default=200000)
+    extra_weight_fee = models.PositiveIntegerField(
+        verbose_name=_('قیمت به ازای هر کیلو اضافه (ریال)'), default=20000)
+    created_datetime = models.DateTimeField(
+        verbose_name=_('تاریخ ایجاد'), auto_now_add=True)
+    updated_datetime = models.DateTimeField(
+        verbose_name=_('تاریخ بروزرسانی'), auto_now=True)
+
     def __str__(self):
         return f'{self.user}: {self.inside_city_price} تومان'
-    
+
 
 class LogisticUnitMetric(models.Model):
     class Meta:
         verbose_name = _('متریک ارسال')
         verbose_name_plural = _('متریک ارسال')
 
-    price_per_kg = models.PositiveIntegerField(verbose_name=_('قیمت هر کیلوگرم'))
-    price_per_extra_kg = models.PositiveIntegerField(verbose_name=_('قیمت هر کیلوگرم اضافه'))
+    price_per_kg = models.PositiveIntegerField(
+        verbose_name=_('قیمت هر کیلوگرم'))
+    price_per_extra_kg = models.PositiveIntegerField(
+        verbose_name=_('قیمت هر کیلوگرم اضافه'))
     is_default = models.BooleanField(default=False, verbose_name=_('پیش فرض؟'))
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
 
@@ -74,16 +93,22 @@ class LogisticUnitConstraintParameter(models.Model):
     class Meta:
         verbose_name = _('پارامتر محدودیت ارسال')
         verbose_name_plural = _('پارامتر محدودیت ارسال')
-        
+
     cities = models.ManyToManyField(City, verbose_name=_('شهرها'))
     products = models.ManyToManyField(Product, verbose_name=_('محصولات'))
-    min_price = models.DecimalField(verbose_name=_('حداقل قیمت'), max_digits=10, decimal_places=2)
-    categories = models.ManyToManyField(NewCategory, verbose_name=_('دسته بندی ها'))
-    max_weight_g = models.PositiveIntegerField(verbose_name=_('حداکثر وزن (گرم)'))
-    max_package_value = models.PositiveIntegerField(verbose_name=_('حداکثر ارزش بسته'))
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
+    min_price = models.DecimalField(verbose_name=_(
+        'حداقل قیمت'), max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(
+        NewCategory, verbose_name=_('دسته بندی ها'))
+    max_weight_g = models.PositiveIntegerField(
+        verbose_name=_('حداکثر وزن (گرم)'))
+    max_package_value = models.PositiveIntegerField(
+        verbose_name=_('حداکثر ارزش بسته'))
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
+
 
 class LogisticUnit(models.Model):
     class Meta:
@@ -91,10 +116,14 @@ class LogisticUnit(models.Model):
         verbose_name_plural = _('واحد ارسال')
 
     name = models.CharField(max_length=50, verbose_name=_('نام  '))
-    description = models.TextField(null=True, blank=True, verbose_name=_('توضیحات'))
-    is_publish = models.BooleanField(default=True, verbose_name=_('منتشر شده؟'))
-    metric = models.ForeignKey(LogisticUnitMetric, on_delete=models.SET_NULL, null=True, verbose_name=_('متریک'))
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
+    description = models.TextField(
+        null=True, blank=True, verbose_name=_('توضیحات'))
+    is_publish = models.BooleanField(
+        default=True, verbose_name=_('منتشر شده؟'))
+    metric = models.ForeignKey(
+        LogisticUnitMetric, on_delete=models.SET_NULL, null=True, verbose_name=_('متریک'))
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
 
@@ -103,11 +132,15 @@ class LogisticUnitConstraint(models.Model):
     class Meta:
         verbose_name = _('محدودیت ارسال')
         verbose_name_plural = _('محدودیت ارسال')
-        
-    logistic_unit = models.ForeignKey(LogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال'))
-    constraint = models.ForeignKey(LogisticUnitConstraintParameter, on_delete=models.SET_NULL, null=True, verbose_name=_('محدودیت'))
-    is_publish = models.BooleanField(default=True, verbose_name=_('منتشر شده؟'))
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
+
+    logistic_unit = models.ForeignKey(
+        LogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال'))
+    constraint = models.ForeignKey(LogisticUnitConstraintParameter,
+                                   on_delete=models.SET_NULL, null=True, verbose_name=_('محدودیت'))
+    is_publish = models.BooleanField(
+        default=True, verbose_name=_('منتشر شده؟'))
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, verbose_name=_('ایجاد کننده'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
 
@@ -116,11 +149,14 @@ class ShopLogisticUnit(models.Model):
     class Meta:
         verbose_name = _('واحد ارسال فروشگاه')
         verbose_name_plural = _('واحد ارسال فروشگاه')
-        
-    shop = models.ForeignKey(Shop, on_delete=models.SET_NULL, null=True, verbose_name=_('فروشگاه'))
-    logistic_unit = models.ForeignKey(LogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال'))
+
+    shop = models.ForeignKey(
+        Shop, on_delete=models.SET_NULL, null=True, verbose_name=_('فروشگاه'))
+    logistic_unit = models.ForeignKey(
+        LogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال'))
     is_active = models.BooleanField(default=True, verbose_name=_('فعال؟'))
-    use_default_setting = models.BooleanField(default=True, verbose_name=_('استفاده از تنظیم پیش فرض؟'))
+    use_default_setting = models.BooleanField(
+        default=True, verbose_name=_('استفاده از تنظیم پیش فرض؟'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
 
@@ -129,9 +165,11 @@ class ShopLogisticUnitConstraint(models.Model):
     class Meta:
         verbose_name = _('محدودیت ارسال فروشگاه')
         verbose_name_plural = _('محدودیت ارسال فروشگاه')
-        
-    shop_logistic_unit = models.ForeignKey(ShopLogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال فروشگاه'))
-    constraint = models.ForeignKey(LogisticUnitConstraintParameter, on_delete=models.SET_NULL, null=True, verbose_name=_('محدودیت'))
+
+    shop_logistic_unit = models.ForeignKey(
+        ShopLogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال فروشگاه'))
+    constraint = models.OneToOneField(LogisticUnitConstraintParameter, on_delete=models.SET_NULL, null=True, verbose_name=_(
+        'محدودیت'), related_name='shop_logistic_unit_constraint')
     is_active = models.BooleanField(default=True, verbose_name=_('فعال؟'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
@@ -141,9 +179,13 @@ class ShopLogisticUnitMetric(models.Model):
     class Meta:
         verbose_name = _('متریک فروشگاه')
         verbose_name_plural = _('متریک فروشگاه')
-        
-    shop_logistic_unit_constraint = models.ForeignKey(ShopLogisticUnit, on_delete=models.SET_NULL, null=True, verbose_name=_('واحد ارسال فروشگاه'))
-    metric = models.ForeignKey(LogisticUnitMetric, on_delete=models.SET_NULL, null=True, verbose_name=_('متریک'))
+
+    shop_logistic_unit_constraint = models.OneToOneField(
+                    ShopLogisticUnitConstraint,
+                    on_delete=models.SET_NULL,
+                    null=True, related_name='shop_logistic_unit_metric',
+                    verbose_name=_('واحد ارسال فروشگاه'))
+    metric = models.OneToOneField(
+        LogisticUnitMetric, on_delete=models.SET_NULL, null=True, verbose_name=_('متریک'))
     created_at = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
     updated_at = models.DateTimeField(_('تاریخ بروزرسانی'), auto_now=True)
-    
--- logistic/serializers.py
+++ logistic/serializers.py
@@ -1,18 +1,22 @@
 from django.db.models import fields
 from rest_framework import serializers
-from logistic.models import Address, LogisticUnitMetric, ShopLogisticUnit, ShopLogisticUnitConstraint, LogisticUnit,LogisticUnitConstraintParameter, ShopLogisticUnitMetric
-from nakhll_market.models import Field, State, BigCity, City
-
+from logistic.models import Address, LogisticUnitConstraint, LogisticUnitMetric, ShopLogisticUnit, ShopLogisticUnitConstraint, LogisticUnit, LogisticUnitConstraintParameter, ShopLogisticUnitMetric
+from nakhll_market.models import Field, Product, State, BigCity, City
 
 
 class AddressSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    state = serializers.SlugRelatedField(slug_field='name', queryset=State.objects.all())
-    big_city = serializers.SlugRelatedField(slug_field='name', queryset=BigCity.objects.all())
-    city = serializers.SlugRelatedField(slug_field='name', queryset=City.objects.all())
+    state = serializers.SlugRelatedField(
+        slug_field='name', queryset=State.objects.all())
+    big_city = serializers.SlugRelatedField(
+        slug_field='name', queryset=BigCity.objects.all())
+    city = serializers.SlugRelatedField(
+        slug_field='name', queryset=City.objects.all())
+
     class Meta:
         model = Address
-        fields = ('id', 'user', 'state', 'big_city', 'city', 'zip_code', 'address', 'phone_number', 'receiver_full_name', 'receiver_mobile_number',)
+        fields = ('id', 'user', 'state', 'big_city', 'city', 'zip_code', 'address',
+                  'phone_number', 'receiver_full_name', 'receiver_mobile_number',)
         read_only_fields = ('id', 'user')
 
     def get_user(self, obj):
@@ -22,58 +26,77 @@ class AddressSerializer(serializers.ModelSerializer):
 class ShopOwnerAddressSerializer(AddressSerializer):
     class Meta:
         model = Address
-        fields = ('id', 'user', 'state', 'big_city', 'city', 'zip_code', 'address', 'receiver_full_name', 'receiver_mobile_number',)
+        fields = ('id', 'user', 'state', 'big_city', 'city', 'zip_code',
+                  'address', 'receiver_full_name', 'receiver_mobile_number',)
         read_only_fields = ('id', 'user')
 
-        
+
 class LogisticUnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = LogisticUnit
         fields = ('id', 'name', 'description',)
         read_only_fields = ('id',)
-        
+
+
 class ShopLogisticUnitSerializer(serializers.ModelSerializer):
     logistic_unit = LogisticUnitSerializer(read_only=True)
+
     class Meta:
         model = ShopLogisticUnit
         fields = ('id', 'is_active', 'logistic_unit')
         read_only_fields = ('id', )
-        
-        
+
+
 class LogisticUnitMetricSerializer(serializers.ModelSerializer):
     class Meta:
         modcel = LogisticUnitMetric
-        fields = ("id","price_per_kg","price_per_extra_kg","is_default")
-        read_only_fields = ("id",)
-            
-        
-class LogisticUnitConstraintParameterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LogisticUnitConstraintParameter
-        fields = ("id","cities","products","min_price","categories","max_weight_g","max_package_value")
+        fields = ("id", "price_per_kg", "price_per_extra_kg", )
         read_only_fields = ("id",)
 
-class LogisticUnitConstraintSerializer(serializers.ModelSerializer):
-    logistic_unit = LogisticUnitSerializer
-    constraint = LogisticUnitConstraintParameter
+
+class LogisticUnitConstraintParameterSerializer(serializers.ModelSerializer):
+    price_per_kg = serializers.IntegerField(required=False, source='\
+                                            shop_logistic_unit_constraint.shop_logistic_unit_metric.price_per_kg\
+                                            ')
+    price_per_extra_kg = serializers.IntegerField(required=False, source='\
+                                            shop_logistic_unit_constraint.shop_logistic_unit_metric.price_per_extra_kg\
+                                            ')
+    cities = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=City.objects.all())
+    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    products = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Product.objects.all())
+
     class Meta:
-        model = ShopLogisticUnitConstraint
-        fields = ("id","is_publish",)
+        model = LogisticUnitConstraintParameter
+        fields = ("id", "cities", "products", "min_price", "categories",
+                  "max_weight_g", "max_package_value", "price_per_kg", "price_per_extra_kg",)
         read_only_fields = ("id",)
-        
-        
+
+# class LogisticUnitConstraintSerializer(serializers.ModelSerializer):
+#     logistic_unit = LogisticUnitSerializer()
+#     constraint = LogisticUnitConstraintParameterSerializer()
+#     class Meta:
+#         model = LogisticUnitConstraint
+#         fields = ("id","is_publish", "logistic_unit", "constraint")
+#         read_only_fields = ("id",)
+
+
 class ShopLogisticUnitConstraintSerializer(serializers.ModelSerializer):
-    shop_logistic_unit = ShopLogisticUnitSerializer
-    constraint = LogisticUnitConstraintParameterSerializer
+    shop_logistic_unit = ShopLogisticUnitSerializer()
+    constraint = LogisticUnitConstraintParameterSerializer()
+
     class Meta:
         model = ShopLogisticUnitConstraint
-        fields = ("id","is_active",)
+        fields = ("id", "is_active",)
         read_only_fields = ("id",)
-        
+
+
 class ShopLogisticUnitMetricSerializer(serializers.ModelSerializer):
     shop_logistic_unit_constraint = ShopLogisticUnitSerializer
     metric = LogisticUnitMetricSerializer
+
     class Meta:
-        model =ShopLogisticUnitMetric
-        fields = ("id",)
+        model = ShopLogisticUnitMetric
+        fields = ("id", "metric", "shop_logistic_unit_constraint",)
         read_only_fields = ("id",)
