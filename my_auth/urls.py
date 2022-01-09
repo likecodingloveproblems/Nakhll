1228e679dea9276bc2c96c816d20672d2fd09e99
Mohsen Hassani <m.hassani.de@gmail.com>
2022-01-09 16:17:08 +0330

    Restore auth urls

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     # path('forget-password-code/', views.ForgetPasswordCode.as_view(), name='forget-password-code'),
     # path('forget-password-data/', views.ForgetPasswordData.as_view(), name='forget-password-data'),
     # logout process
-    # re_path(r'^logout/$', views.logout_, name='logout'),
+    re_path(r'^logout/$', views.logout_, name='logout'),
     # authenticate user
-    # re_path(r'^get-phone/$', views.GetPhone.as_view(), name='get-phone'),
-    # re_path(r'^register/$', views.Register.as_view(), name='register'),
-    # re_path(r'^login-password/$', views.LoginPassword.as_view(), name='login-password'),
-    # re_path(r'^login-code/$', views.LoginCode.as_view(), name='login-code'),
-    # re_path(r'^get-code/$', views.GetCode.as_view(), name='get-code'),
+    re_path(r'^get-phone/$', views.GetPhone.as_view(), name='get-phone'),
+    re_path(r'^register/$', views.Register.as_view(), name='register'),
+    re_path(r'^login-password/$', views.LoginPassword.as_view(), name='login-password'),
+    re_path(r'^login-code/$', views.LoginCode.as_view(), name='login-code'),
+    re_path(r'^get-code/$', views.GetCode.as_view(), name='get-code'),
 ]
