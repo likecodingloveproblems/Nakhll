23314fc41910d2c0a193a2d0d551705f61362272
Mohsen Hassani <m.hassani.de@gmail.com>
2022-01-22 12:54:17 +0330

    Fix issue in creating shop

--- logistic/managers.py
+++ logistic/managers.py
@@ -13,9 +13,8 @@ class AddressManager(models.Manager):
 class ShopLogisticUnitManager(models.Manager):
     
     def generate_shop_logistic_units(self, shop):
-        logo_path = 'static/Pictures/lu_free.png'
-        logo_file = File(open(logo_path, 'rb'))
-        slu = self.create(shop=shop, name='ارسال رایگان', is_active=False, logo=logo_file)
+        slu = self.create(shop=shop, name='ارسال رایگان', is_active=False,
+                          logo_type=models.ShopLogisticUnit.LogoType.FREE)
         sluc = models.ShopLogisticUnitConstraint.objects.create(
             shop_logistic_unit=slu
         )
@@ -23,9 +22,8 @@ class ShopLogisticUnitManager(models.Manager):
             shop_logistic_unit=slu, price_per_kilogram=0, price_per_extra_kilogram=0
         )
 
-        logo_path = 'static/Pictures/lu_delivery.png'
-        logo_file = File(open(logo_path, 'rb'))
-        slu = self.create(shop=shop, name='پیک', is_active=False, logo=logo_file)
+        slu = self.create(shop=shop, name='پیک', is_active=False,
+                          logo_type=models.ShopLogisticUnit.LogoType.DELIVERY)
         sluc = models.ShopLogisticUnitConstraint.objects.create(
             shop_logistic_unit=slu,
         )
@@ -37,9 +35,8 @@ class ShopLogisticUnitManager(models.Manager):
             shop_logistic_unit=slu, price_per_kilogram=0, price_per_extra_kilogram=0
         )
   
-        logo_path = 'static/Pictures/lu_pad.png'
-        logo_file = File(open(logo_path, 'rb'))
-        slu = self.create(shop=shop, name='پسکرایه', is_active=True, logo=logo_file)
+        slu = self.create(shop=shop, name='پسکرایه', is_active=True,
+                          logo_type=models.ShopLogisticUnit.LogoType.PAD)
         sluc = models.ShopLogisticUnitConstraint.objects.create(
             shop_logistic_unit=slu
         )
@@ -47,9 +44,8 @@ class ShopLogisticUnitManager(models.Manager):
             shop_logistic_unit=slu, price_per_kilogram=0, price_per_extra_kilogram=0
         )
 
-        logo_path = 'static/Pictures/lu_post_pishtaz.png'
-        logo_file = File(open(logo_path, 'rb'))
-        slu = self.create(shop=shop, name='پست پیشتاز', is_active=True, logo=logo_file)
+        slu = self.create(shop=shop, name='پست پیشتاز', is_active=True,
+                          logo_type=models.ShopLogisticUnit.LogoType.PPOST)
         sluc = models.ShopLogisticUnitConstraint.objects.create(
             shop_logistic_unit=slu, max_weight=40
         )
@@ -57,9 +53,8 @@ class ShopLogisticUnitManager(models.Manager):
             shop_logistic_unit=slu, price_per_kilogram=150000, price_per_extra_kilogram=25000
         )
         
-        logo_path = 'static/Pictures/lu_post_sefareshi.png'
-        logo_file = File(open(logo_path, 'rb'))
-        slu = self.create(shop=shop, name='پست سفارشی', is_active=True, logo=logo_file)
+        slu = self.create(shop=shop, name='پست سفارشی', is_active=True,
+                          logo_type=models.ShopLogisticUnit.LogoType.SPOST)
         sluc = models.ShopLogisticUnitConstraint.objects.create(
             shop_logistic_unit=slu, max_weight=40
         )
