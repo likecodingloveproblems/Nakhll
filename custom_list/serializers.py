e5cbcff898a01eae72777debf20a411bc51f2e9f
Mohsen Hassani <m.hassani.de@gmail.com>
2022-02-05 13:45:49 +0330

    Rename product to products in favorite list

--- custom_list/serializers.py
+++ custom_list/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
 from nakhll_market.serializers import ProductSerializer, UserSerializer
+from nakhll_market.models import Product
 from custom_list.models import Favorite
 
 
 class UserFavoriteProductSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(many=True, read_only=False)
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    products = ProductSerializer(many=True, read_only=True)
     user = UserSerializer(many=False, read_only=True)
     class Meta:
         model = Favorite
-        fields = ['product', 'user']
+        fields = ['product', 'products', 'user']
 
     def save(self, **kwargs):
         fav_list = kwargs.pop('fav_list')
-        validated_data = {**self.validated_data, 'user': fav_list.user}
-        return self.update(self.instance, validated_data)
+        fav_list.products.add(self.validated_data['product_id'])
+        return fav_list
