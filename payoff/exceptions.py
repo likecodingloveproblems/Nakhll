a3389dc56b344e41806ae30a6627597a5f3901d7
Mohsen Hassani <m.hassani.de@gmail.com>
2022-02-05 13:31:29 +0330

    Change inharitance of payoff exceptions to rest exception

--- payoff/exceptions.py
+++ payoff/exceptions.py
@@ -1,29 +1,30 @@
-class NoTransactionException(Exception):
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.exceptions import APIException
+class NoTransactionException(APIException):
     """
-    Exception raised when a transaction is not found.
+    Validation raised when a transaction is not found.
     """
+    status_code = HTTP_400_BAD_REQUEST
 
-class NoCompletePaymentMethodException(Exception):
+class NoCompletePaymentMethodException(APIException):
     """
-    Exception raised when complete_payment method for given model is not found.
+    Validation raised when complete_payment method for given model is not found.
     """
+    status_code = HTTP_400_BAD_REQUEST
 
-class NoAddressException(Exception):
-    """
-    Exception raised when no address is set for invoice.
-    """
+class NoAddressException(APIException):
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = {'error': 'آدرس خریدار را تکمیل کنید'}
 
-class OutOfPostRangeProductsException(Exception):
-    """
-    Exception raised when one or more products cannot be sent to user address
-    """
+class OutOfPostRangeProductsException(APIException):
+    invalid_products = []
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = 'برخی از محصولات خریداری شده خارج از محدوده ارسال می باشند'
 
-class InvoiceExpiredException(Exception):
-    """
-    Exception raised when invoice is expired.
-    """
+class InvoiceExpiredException(APIException):
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = {'error': 'فاکتور منقضی شده است'}
 
-class InvalidInvoiceStatusException(Exception):
-    """
-    Exception raised when invoice status is invalid.
-    """
+class InvalidInvoiceStatusException(APIException):
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = {'error': 'فاکتور در حال حاضر قابل پرداخت نیست'}
