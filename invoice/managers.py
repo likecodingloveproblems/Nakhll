5a8c74b71e509fbd2bb724d54dd9483e73f2c2f2
Mohsen Hassani <m.hassani.de@gmail.com>
2022-03-03 13:31:40 +0330

    Fix issue in importing invoice models in invoice manager

--- invoice/managers.py
+++ invoice/managers.py
@@ -1,4 +1,4 @@
-import invoice.models as models
+import invoice.models as invoice_models
 import jdatetime
 from django.db import models
 from django.db.models.aggregates import Sum
@@ -9,20 +9,20 @@ from django.db.models.functions.comparison import Coalesce
 
 class AccountingManager(models.Manager):
     def completed_user_invoices(self, user):
-        statuses = models.Invoice.Statuses
+        statuses = invoice_models.Invoice.Statuses
         return self.filter(items__product__FK_Shop__FK_ShopManager=user,
                            status__in=[statuses.COMPLETED, statuses.CANCELED]
                            ).order_by('-created_datetime')
 
     def completed_user_shop_invoices(self, user, shop_slug):
-        statuses = models.Invoice.Statuses
+        statuses = invoice_models.Invoice.Statuses
         return self.filter(items__product__FK_Shop__FK_ShopManager=user,
                            items__product__FK_Shop__Slug=shop_slug, status__in=[
                                statuses.COMPLETED, statuses.CANCELED]
                            ).order_by('-created_datetime')
 
     def uncompleted_user_invoices(self, user):
-        statuses = models.Invoice.Statuses
+        statuses = invoice_models.Invoice.Statuses
         return self.filter(items__product__FK_Shop__FK_ShopManager=user,
                            status__in=[
                                statuses.AWAIT_CUSTOMER_APPROVAL, statuses.AWAIT_SHOP_CHECKOUT,
@@ -30,7 +30,7 @@ class AccountingManager(models.Manager):
                            ]).order_by('-created_datetime')
 
     def uncompleted_user_shop_invoices(self, user, shop_slug):
-        statuses = models.Invoice.Statuses
+        statuses = invoice_models.Invoice.Statuses
         return self.filter(items__product__FK_Shop__FK_ShopManager=user,
                            items__product__FK_Shop__Slug=shop_slug, status__in=[
                                statuses.AWAIT_CUSTOMER_APPROVAL, statuses.AWAIT_SHOP_CHECKOUT,
@@ -40,7 +40,7 @@ class AccountingManager(models.Manager):
     def unconfirmed_user_shop_invoices(self, user, shop_slug):
         return self.filter(items__product__FK_Shop__FK_ShopManager=user,
                            items__product__FK_Shop__Slug=shop_slug,
-                           items__status=models.InvoiceItem.ItemStatuses.AWAIT_SHOP_APPROVAL
+                           items__status=invoice_models.InvoiceItem.ItemStatuses.AWAIT_SHOP_APPROVAL
                            ).order_by('-created_datetime')
 
     def shop_invoices(self, shop_id):
@@ -57,7 +57,7 @@ class AccountingManager(models.Manager):
 
 class InvoiceItemManager(models.Manager):
     def uncompleted_user_shop_factors(self, user, shop_slug):
-        statuses = models.InvoiceItem.ItemStatuses
+        statuses = invoice_models.InvoiceItem.ItemStatuses
         return self.filter(product__FK_Shop__FK_ShopManager=user,
                            product__FK_Shop__Slug=shop_slug, status__in=[
                                statuses.AWAIT_CUSTOMER_APPROVAL, statuses.AWAIT_SHOP_CHECKOUT,
@@ -65,7 +65,7 @@ class InvoiceItemManager(models.Manager):
                            ]).order_by('-added_datetime')
 
     def completed_user_shop_factors(self, user, shop_slug):
-        statuses = models.InvoiceItem.ItemStatuses
+        statuses = invoice_models.InvoiceItem.ItemStatuses
         return self.filter(
             product__FK_Shop__FK_ShopManager=user,
             product__FK_Shop__Slug=shop_slug,
@@ -75,7 +75,7 @@ class InvoiceItemManager(models.Manager):
     def user_total_sell(self, user):
         return self.filter(
             product__FK_Shop__FK_ShopManager=user,
-            invoice__status__in=[models.Invoice.Statuses.COMPLETED]).aggregate(
+            invoice__status__in=[invoice_models.Invoice.Statuses.COMPLETED]).aggregate(
             amont=Sum('product__Price'))
 
     def current_week_user_total_sell(self, user, shop_slug):
@@ -83,7 +83,7 @@ class InvoiceItemManager(models.Manager):
         current_week_start_date = now - jdatetime.timedelta(days=4)
         return self.filter(
             product__FK_Shop__FK_ShopManager=user,
-            invoice__status=models.Invoice.Statuses.COMPLETED,
+            invoice__status=invoice_models.Invoice.Statuses.COMPLETED,
             product__FK_Shop__Slug=shop_slug,
             invoice__created_datetime__gt=str(
                 current_week_start_date.togregorian())).aggregate(
@@ -96,7 +96,7 @@ class InvoiceItemManager(models.Manager):
             days=7)
         return self.filter(
             product__FK_Shop__FK_ShopManager=user,
-            invoice__status=models.Invoice.Statuses.COMPLETED,
+            invoice__status=invoice_models.Invoice.Statuses.COMPLETED,
             invoice__created_datetime__gt=str(
                 last_week_start_date.togregorian()),
             invoice__created_datetime__lt=str(
@@ -108,7 +108,7 @@ class InvoiceItemManager(models.Manager):
         now = jdatetime.datetime.now()
         week_start_date = now - jdatetime.timedelta(days=4)
         return self.filter(product__FK_Shop__FK_ShopManager=user,
-                           invoice__status=models.Invoice.Statuses.COMPLETED,
+                           invoice__status=invoice_models.Invoice.Statuses.COMPLETED,
                            product__FK_Shop__Slug=shop_slug,
                            invoice__created_datetime__gt=str(
                                week_start_date.togregorian())).aggregate(
