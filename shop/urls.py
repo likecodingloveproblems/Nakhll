5fc607f9612c6a76a11a731dcb57ac41bf786d42
Mohsen Hassani <m.hassani.de@gmail.com>
2022-03-03 09:18:29 +0330

    Change urls to plural due to rest api convention

--- shop/urls.py
+++ shop/urls.py
@@ -3,14 +3,22 @@ from rest_framework.routers import DefaultRouter
 from .api import ShopAdvertisementViewSet, ShopFeatureViewSet, ShopFeatureInvoiceViewSet, ShopLandingViewSet, PinnedURLViewSet, ShopViewSet
 
 shop_router = DefaultRouter()
-shop_router.register('', ShopViewSet, basename='shop')
-shop_router.register('feature', ShopFeatureViewSet, basename='shop_feature')
-shop_router.register('feature_invoice', ShopFeatureInvoiceViewSet, basename='shop_feature_invoice')
-shop_router.register('advertisement', ShopAdvertisementViewSet, basename='shop_advertisement')
-shop_router.register('landing/(?P<shop__Slug>[^/.]+)', ShopLandingViewSet, basename='shop_landing')
-shop_router.register('pinned_url', PinnedURLViewSet, basename='pinned_url')
+shop_router.register('', ShopViewSet, basename='shops')
+shop_router.register('features', ShopFeatureViewSet, basename='shop_features')
+shop_router.register(
+    'feature-invoices',
+    ShopFeatureInvoiceViewSet,
+    basename='shop_feature_invoices')
+shop_router.register(
+    'advertisements',
+    ShopAdvertisementViewSet,
+    basename='shop_advertisements')
+shop_router.register(
+    'landings/(?P<shop__Slug>[^/.]+)',
+    ShopLandingViewSet,
+    basename='shop_landings')
+shop_router.register('pinned_urls', PinnedURLViewSet, basename='pinned_urls')
 
 urlpatterns = [
     path('', include(shop_router.urls)),
 ]
-
