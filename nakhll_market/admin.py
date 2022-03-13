1c85a888f9f0f5bbf2769f286b0d5f6983a9da15
Pourya <pourya.mansouri@hotmail.com>
2022-03-13 14:01:46 +0330

    add tag and produt_tag model to django admin

--- nakhll_market/admin.py
+++ nakhll_market/admin.py
@@ -4,9 +4,9 @@ from django.http import HttpRequest, HttpResponse
 from django.utils.timezone import localtime
 from .models import (LandingPageSchema, Category, ShopPageSchema,
                      LandingImage, LandingPage,
-                     Shop, 
-                     Product, ProductBanner, Profile, 
-                     Alert, DashboardBanner, Slider)
+                     Shop,
+                     Product, ProductBanner, Profile,
+                     Alert, DashboardBanner, Slider, Tag, ProductTag)
 from django.contrib import admin
 from django.contrib.auth.models import Permission
 
@@ -14,140 +14,182 @@ admin.site.site_header = 'مدیریت بازار نخل '
 
 # enable django permission setting in admin panel to define custom permissions
 admin.site.register(Permission)
-    
-#profile admin panel
+
+
+# profile admin panel
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display=('FK_User','MobileNumber','BrithDay','UserReferenceCode','Point')
+    list_display = ('FK_User', 'MobileNumber', 'BrithDay', 'UserReferenceCode', 'Point')
     readonly_fields = ('UserReferenceCode',)
-    list_filter=('BrithDay',)
-    ordering=['ID','BrithDay','Point','UserReferenceCode']
-    search_fields=('MobileNumber','UserReferenceCode',)
-#-------------------------------------------------
-#market admin panel
+    list_filter = ('BrithDay',)
+    ordering = ['ID', 'BrithDay', 'Point', 'UserReferenceCode']
+    search_fields = ('MobileNumber', 'UserReferenceCode',)
+
+
+# -------------------------------------------------
+# market admin panel
 
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    list_display=('Title','Slug','City','State','Point','DateCreate','Available','Publish',)
-    list_filter=('City','State','Publish','Available','DateCreate','DateUpdate')
-    search_fields=('Title','Slug')
-    ordering=['ID','DateCreate','DateUpdate']
-    raw_id_fields = ('FK_ShopManager', )
-    def DateCreate(self,obj):
+    list_display = ('Title', 'Slug', 'City', 'State', 'Point', 'DateCreate', 'Available', 'Publish',)
+    list_filter = ('City', 'State', 'Publish', 'Available', 'DateCreate', 'DateUpdate')
+    search_fields = ('Title', 'Slug')
+    ordering = ['ID', 'DateCreate', 'DateUpdate']
+    raw_id_fields = ('FK_ShopManager',)
+
+    def DateCreate(self, obj):
         return localtime(obj.DateCreate).strftime('%Y-%m-%d %H:%M:%S')
-    def DateUpdate(self,obj):
+
+    def DateUpdate(self, obj):
         return localtime(obj.DateUpdate).strftime('%Y-%m-%d %H:%M:%S')
-#-------------------------------------------------
+
+
+# -------------------------------------------------
 class ProductBannerInline(admin.StackedInline):
-    model=ProductBanner
-    extra=1
+    model = ProductBanner
+    extra = 1
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display=('Title','Slug','Bio','Price','Status','DateCreate','Publish')
-    list_filter=('Status','Publish','Available','DateCreate','DateUpdate')
-    search_fields=('Title','Slug','Description','Bio','Story')
-    ordering=['ID','DateCreate','DateUpdate']
-    inlines=[ProductBannerInline,]
+    list_display = ('Title', 'Slug', 'Bio', 'Price', 'Status', 'DateCreate', 'Publish')
+    list_filter = ('Status', 'Publish', 'Available', 'DateCreate', 'DateUpdate')
+    search_fields = ('Title', 'Slug', 'Description', 'Bio', 'Story')
+    ordering = ['ID', 'DateCreate', 'DateUpdate']
+    inlines = [ProductBannerInline, ]
 
-    def DateCreate(self,obj):
+    def DateCreate(self, obj):
         return localtime(obj.DateCreate).strftime('%Y-%m-%d %H:%M:%S')
-    def DateUpdate(self,obj):
+
+    def DateUpdate(self, obj):
         return localtime(obj.DateUpdate).strftime('%Y-%m-%d %H:%M:%S')
-    def change_view(self, request: HttpRequest, object_id: str, form_url: str='', extra_context: Optional[Dict[str, bool]]=None) -> HttpResponse:
+
+    def change_view(self, request: HttpRequest, object_id: str, form_url: str = '',
+                    extra_context: Optional[Dict[str, bool]] = None) -> HttpResponse:
         if request.user.groups.filter(name='Photo-compress').exists():
-            self.fields = ('Image','NewImage')
+            self.fields = ('Image', 'NewImage')
         else:
             self.fields = None
         return super().change_view(request, object_id, form_url=form_url, extra_context=extra_context)
-#-------------------------------------------------
+
+
+# -------------------------------------------------
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display=('id', 'name', 'order', 'slug', 'description', 'parent', 'available')
-    list_filter=('parent','available')
-    ordering=('-parent', 'id', )
+    list_display = ('id', 'name', 'order', 'slug', 'description', 'parent', 'available')
+    list_filter = ('parent', 'available')
+    ordering = ('-parent', 'id',)
 
 
-#-------------------------------------------------
-#Alert admin panel
+# -------------------------------------------------
+# Alert admin panel
 @admin.register(Alert)
 class AlertAdmin(admin.ModelAdmin):
-    list_display=('Part','Slug','Seen','DateCreate','Description',)
-    list_filter=('Seen','Status','DateCreate','DateCreate','DateUpdate')
-    search_fields=('FK_Field','Description','Content',)
-    ordering=['id','DateCreate',]
-    def DateCreate(self,obj):
+    list_display = ('Part', 'Slug', 'Seen', 'DateCreate', 'Description',)
+    list_filter = ('Seen', 'Status', 'DateCreate', 'DateCreate', 'DateUpdate')
+    search_fields = ('FK_Field', 'Description', 'Content',)
+    ordering = ['id', 'DateCreate', ]
+
+    def DateCreate(self, obj):
         return localtime(obj.DateCreate).strftime('%Y-%m-%d %H:%M:%S')
-    def DateUpdate(self,obj):
+
+    def DateUpdate(self, obj):
         return localtime(obj.DateUpdate).strftime('%Y-%m-%d %H:%M:%S')
+
 
 @admin.register(ProductBanner)
 class ProductBanner(admin.ModelAdmin):
-    list_display=('FK_Product','Title', 'DateCreate', 'DateUpdate')
-    list_filter=('FK_Product','Title','DateCreate','DateUpdate')
-    search_fields=('FK_Product__Title',)
-    ordering=('DateCreate',)
-    def DateCreate(self,obj):
+    list_display = ('FK_Product', 'Title', 'DateCreate', 'DateUpdate')
+    list_filter = ('FK_Product', 'Title', 'DateCreate', 'DateUpdate')
+    search_fields = ('FK_Product__Title',)
+    ordering = ('DateCreate',)
+
+    def DateCreate(self, obj):
         return localtime(obj.DateCreate).strftime('%Y-%m-%d %H:%M:%S')
-    def DateUpdate(self,obj):
+
+    def DateUpdate(self, obj):
         return localtime(obj.DateUpdate).strftime('%Y-%m-%d %H:%M:%S')
+
 
 @admin.register(DashboardBanner)
 class DashboardBannerAdmin(admin.ModelAdmin):
-    list_display=('image','url','staff_user','created_datetime','publish_status')
-    list_filter=('staff_user','created_datetime','publish_status')
-    search_fields=('url',)
-    ordering=['id','created_datetime','publish_status']
-    def created_datetime(self,obj):
+    list_display = ('image', 'url', 'staff_user', 'created_datetime', 'publish_status')
+    list_filter = ('staff_user', 'created_datetime', 'publish_status')
+    search_fields = ('url',)
+    ordering = ['id', 'created_datetime', 'publish_status']
+
+    def created_datetime(self, obj):
         return localtime(obj.created_datetime).strftime('%Y-%m-%d %H:%M:%S')
     # inlines=[AttrProductInline, ProductBannerInline, ProductMovieInline]
 
+
 @admin.register(LandingPageSchema)
 class LandingPageSchemaAdmin(admin.ModelAdmin):
-    list_display=('component_type', 'title', 'subtitle', 'image', 'url', 'data', 'order', 'publish_status')
-    list_filter=('publish_status','created_datetime', 'order')
-    search_fields=('title','component_type', 'subtitle', 'url', 'data')
-    ordering=['publish_status', 'created_datetime']
-    def created_datetime(self,obj):
+    list_display = ('component_type', 'title', 'subtitle', 'image', 'url', 'data', 'order', 'publish_status')
+    list_filter = ('publish_status', 'created_datetime', 'order')
+    search_fields = ('title', 'component_type', 'subtitle', 'url', 'data')
+    ordering = ['publish_status', 'created_datetime']
+
+    def created_datetime(self, obj):
         return localtime(obj.created_datetime).strftime('%Y-%m-%d %H:%M:%S')
+
 
 @admin.register(ShopPageSchema)
 class ShopPageSchemaAdmin(admin.ModelAdmin):
-    list_display=('shop','component_type', 'title', 'subtitle', 'image', 'url', 'data', 'order', 'publish_status')
-    list_filter=('shop','publish_status','created_datetime', 'order')
-    search_fields=('shop','title','component_type', 'subtitle', 'url', 'data')
-    ordering=['publish_status', 'created_datetime']
-    def created_datetime(self,obj):
+    list_display = ('shop', 'component_type', 'title', 'subtitle', 'image', 'url', 'data', 'order', 'publish_status')
+    list_filter = ('shop', 'publish_status', 'created_datetime', 'order')
+    search_fields = ('shop', 'title', 'component_type', 'subtitle', 'url', 'data')
+    ordering = ['publish_status', 'created_datetime']
+
+    def created_datetime(self, obj):
         return localtime(obj.created_datetime).strftime('%Y-%m-%d %H:%M:%S')
 
-        
+
 @admin.register(LandingPage)
 class LandingPageAdmin(admin.ModelAdmin):
-    list_display=('slug', 'status', 'staff', 'created_at', 'updated_at')
-    list_filter=('status', )
-    search_fields=('slug','page_data')
-    ordering=['created_at']
-    def created_datetime(self,obj):
+    list_display = ('slug', 'status', 'staff', 'created_at', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('slug', 'page_data')
+    ordering = ['created_at']
+
+    def created_datetime(self, obj):
         return localtime(obj.created_datetime).strftime('%Y-%m-%d %H:%M:%S')
+
 
 @admin.register(LandingImage)
 class LandingImageAdmin(admin.ModelAdmin):
-    list_display=('image', 'staff', 'created_at', 'updated_at', 'landing_page')
-    list_filter=('landing_page', )
-    ordering=['created_at']
-    def created_datetime(self,obj):
+    list_display = ('image', 'staff', 'created_at', 'updated_at', 'landing_page')
+    list_filter = ('landing_page',)
+    ordering = ['created_at']
+
+    def created_datetime(self, obj):
         return localtime(obj.created_datetime).strftime('%Y-%m-%d %H:%M:%S')
 
-        
+
 @admin.register(Slider)
 class SliderAdmin(admin.ModelAdmin):
-    list_display=('Title','Description','Location','DateCreate','Publish')
-    list_filter=('Location','DateCreate','DtatUpdate','Publish')
-    ordering=['DateCreate','id','Publish','Title','Location']
-    def DateCreate(self,obj):
+    list_display = ('Title', 'Description', 'Location', 'DateCreate', 'Publish')
+    list_filter = ('Location', 'DateCreate', 'DtatUpdate', 'Publish')
+    ordering = ['DateCreate', 'id', 'Publish', 'Title', 'Location']
+
+    def DateCreate(self, obj):
         return localtime(obj.DateCreate).strftime('%Y-%m-%d %H:%M:%S')
-    def DtatUpdate(self,obj):
+
+    def DtatUpdate(self, obj):
         return localtime(obj.DtatUpdate).strftime('%Y-%m-%d %H:%M:%S')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'shop']
+    list_filter = ['shop']
+    search_fields = ['name']
+
+
+@admin.register(ProductTag)
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag', 'product']
+    search_fields = ['tag__name']
+    autocomplete_fields = ['product', 'tag']
