4b46698c5a371637bf1e4489aedc9f050d4752e2
pourya <pourya.mansouri@hotmail.com>
2022-04-26 10:25:19 +0430

    filter price  for each  request

--- nakhll_market/api.py
+++ nakhll_market/api.py
@@ -345,14 +345,14 @@ class ProductsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
             When(OldPrice__gt=0, then=(
                     (F('OldPrice') - F('Price')) * 100 / F('OldPrice'))
                  ), default=0)).order_by('-is_available', '-category_id')
-        self.Max_price = res.aggregate(Max('Price'))['Price__max']
-        self.Min_price = res.aggregate(Min('Price'))['Price__min']
         return res
 
     def get_paginated_response(self, data):
         res = super().get_paginated_response(data)
-        res.data['max_price'] = self.Max_price
-        res.data['min_price'] = self.Min_price
+        query = res.data['results'].copy()
+        sorted_query = sorted(query, key=lambda x: x['Price'])
+        res.data['max_price'] = sorted_query[-1]['Price']
+        res.data['min_price'] = sorted_query[0]['Price']
         return res
 
 
