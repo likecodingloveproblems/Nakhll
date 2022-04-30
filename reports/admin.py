63ba64bb225a8dd44add78e069165ad01d822891
pourya <pourya.mansouri@hotmail.com>
2022-04-30 14:25:25 +0430

    proxy class

--- reports/admin.py
+++ reports/admin.py
@@ -3,32 +3,51 @@ from panel_admins.reports import reports_admin_site
 # Register your models here.
 from .models import InvoiceItemReport, InoviceProxy
 from import_export.admin import ExportActionMixin
+from import_export import resources
+from import_export.fields import Field
+
+
+class InvoiceItemReportResource(resources.ModelResource):
+    product_title = Field()
+
+    def dehydrate_product_title(self, InvoiceItemReport):
+        return InvoiceItemReport.product.Title
+
+    class Meta:
+        model = InvoiceItemReport
+        exclude = ('product',)
+
+    def dehydrate_product_name(self, invoice_item_report):
+        return invoice_item_report.product.Title
 
 
 @admin.register(InvoiceItemReport, site=reports_admin_site)
-class InvoiceItemReportAdmin(ExportActionMixin,admin.ModelAdmin):
+class InvoiceItemReportAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('id', 'invoice_number', 'invoice_status', 'product_name', 'product_shop')
-    list_filter = ( 'status',)
+    list_filter = ('status',)
+    resource_class = InvoiceItemReportResource
     ordering = ('-id',)
 
     # product items
-    def product_name(self,obj):
+    def product_name(self, obj):
         return obj.product.Title
+
     product_name.short_description = 'اسم محصول'
 
-    def product_shop(self,obj):
+    def product_shop(self, obj):
         return obj.product.FK_Shop.Title
+
     product_shop.short_description = 'فروشگاه'
 
-
-
     # invoice items
-    def invoice_status(self,obj):
+    def invoice_status(self, obj):
         return obj.invoice.status
+
     invoice_status.short_description = 'وضعیت فاکتور'
 
-    def invoice_number(self,obj):
+    def invoice_number(self, obj):
         return obj.invoice.id
+
     invoice_number.short_description = 'شماره فاکتور'
 
     def get_queryset(self, request):
