9418f397dd730f681912b469bfe89f5401aa2cce
pourya <pourya.mansouri@hotmail.com>
2022-04-30 09:10:11 +0430

    invoiceItem reports add

--- reports/admin.py
+++ reports/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
-from panel_admins.reports import ReportsAdminSite
+from panel_admins.reports import reports_admin_site
 # Register your models here.
-from invoice.models import InvoiceItem
+from .models import InvoiceItemReport
 
 
-@admin.register(InvoiceItem, site=ReportsAdminSite)
-class InvoiceItemAdmin(admin.ModelAdmin):
+@admin.register(InvoiceItemReport, site=reports_admin_site)
+class InvoiceItemReportAdmin(admin.ModelAdmin):
     list_display = ('id', 'invoice', 'product',)
     list_filter = ('invoice', 'product')
     search_fields = ('invoice', 'product')
-    ordering = ('id',)
-    readonly_fields = ('id',)
+    ordering = ('-id',)
+
     def has_add_permission(self, request):
         return False
+
     def has_delete_permission(self, request, obj=None):
         return False
+
     def has_change_permission(self, request, obj=None):
         return False
