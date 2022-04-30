69787516fd9f23f3374edab3cc791403479a1b44
pourya <pourya.mansouri@hotmail.com>
2022-04-30 09:04:04 +0430

    create proxy model from InvoicItems

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from invoice.models import InvoiceItem
+
+
+class InvoiceItemReport(InvoiceItem):
+    """
+    This class is used to create a report for the invoice items.
+    """
+    class Meta:
+        proxy = True
+        verbose_name = 'گزارش محصولات فاکتور شده'
+        verbose_name_plural = 'گزارش محصولات فاکتور شده'
