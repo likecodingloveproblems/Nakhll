c5511933bef09681d7ef3ab2bb3a7f8c570dbb78
pourya <pourya.mansouri@hotmail.com>
2022-04-30 13:34:32 +0430

    rename titles

--- reports/admin.py
+++ reports/admin.py
@@ -2,31 +2,34 @@ from django.contrib import admin
 from panel_admins.reports import reports_admin_site
 # Register your models here.
 from .models import InvoiceItemReport, InoviceProxy
+from import_export.admin import ExportActionMixin
 
 
 @admin.register(InvoiceItemReport, site=reports_admin_site)
-class InvoiceItemReportAdmin(admin.ModelAdmin):
-    list_display = ('id', 'invoice', 'invoice_status', 'product_name', 'product_shop')
-    list_filter = ('invoice', 'product')
-    search_fields = ('invoice', 'product')
+class InvoiceItemReportAdmin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('id', 'invoice_number', 'invoice_status', 'product_name', 'product_shop')
+    list_filter = ( 'status',)
     ordering = ('-id',)
 
     # product items
-    @staticmethod
-    def product_name(obj):
+    def product_name(self,obj):
         return obj.product.Title
     product_name.short_description = 'اسم محصول'
 
-    @staticmethod
-    def product_shop(obj):
+    def product_shop(self,obj):
         return obj.product.FK_Shop.Title
+    product_shop.short_description = 'فروشگاه'
 
 
 
     # invoice items
-    @staticmethod
-    def invoice_status(obj):
+    def invoice_status(self,obj):
         return obj.invoice.status
+    invoice_status.short_description = 'وضعیت فاکتور'
+
+    def invoice_number(self,obj):
+        return obj.invoice.id
+    invoice_number.short_description = 'شماره فاکتور'
 
     def get_queryset(self, request):
         qs = super(InvoiceItemReportAdmin, self).get_queryset(request)
