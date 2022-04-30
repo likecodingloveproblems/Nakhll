7553d8baa17ebbb233912ad7a111fe3ca6bb56ec
pourya <pourya.mansouri@hotmail.com>
2022-04-30 08:33:05 +0430

    permission to reports panel

---
+++ panel_admins/reports.py
@@ -0,0 +1,20 @@
+from django.contrib.admin import AdminSite
+
+
+class ReportsAdminSite(AdminSite):
+    site_header = "Panel for get reports"
+    site_title = "Nakhll reports panel"
+    index_title = "Welcome to Nagll reports panel"
+
+    def has_permission(self, request):
+        """
+        Return True if the given HttpRequest has permission to view
+        *at least one* page in the admin site.
+        """
+        groups = request.user.groups.all()
+        if request.user.is_active and request.user.is_staff and 'reports' in [group.name for group in groups]:
+            return True
+        return False
+
+
+reports_admin_site = ReportsAdminSite(name='reports_admin')
