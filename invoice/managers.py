25b4bf995a09a99f36751637a913c7646442e2c4
pourya <pourya.mansouri@hotmail.com>
2022-05-08 21:56:51 +0430

    fix bugs shop dashboard

--- invoice/managers.py
+++ invoice/managers.py
@@ -64,7 +64,7 @@ class AccountingManager(models.Manager):
         return self.annotate(
             coupon_price=Coalesce(
                 Sum('coupon_usages__price_applied',
-                    output_field=DecimalField()), Value(0)),
+                    output_field=DecimalField()), Value(0.0)),
             price=F('invoice_price_with_discount') +
             F('logistic_price') -
             F('coupon_price'),
