f2bf7596980a6fbc04772aa8e9e1d836addce29a
Pourya Mansouri <pourya.mansouri@hotmail.com>
2022-05-17 05:04:56 +0430

    change image and category errors message

--- nakhll_market/serializers.py
+++ nakhll_market/serializers.py
@@ -403,10 +403,13 @@ class ProductBannerWriteSerializer(serializers.ModelSerializer):
 
 class ProductOwnerWriteSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(
-        read_only=False, many=False, queryset=Category.objects.all())
-    product_tags = ProductTagWriteSerializer(
-        many=True, read_only=False, required=False)
-    Image = Base64ImageField(max_length=None, use_url=True)
+        read_only=False, many=False, queryset=Category.objects.all(),required=True,
+        error_messages={'required': ' کتگوری را انتخاب کنید','null':'کتگوری را انتخاب کنید'})
+    product_tags = ProductTagWriteSerializer(many=True, read_only=False, required=False)
+    Image = Base64ImageField(max_length=None, use_url=True,error_messages={
+        'null': 'لطفا تصویر را انتخاب کنید',
+        'required': 'لطفا تصویر را انتخاب کنید'
+    })
     Product_Banner = ProductBannerWriteSerializer(many=True, read_only=False)
     post_range = serializers.PrimaryKeyRelatedField(
         source='post_range_cities', read_only=False, many=True,
