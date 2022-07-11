861e17d2f84e288032984b0a40e0ba0517e9451c
Mohsen Hassani <m.hassani.de@gmail.com>
2022-07-11 21:13:52 +0400

    Fix issue in connecting to SEP IPG

--- nakhll/setup.py
+++ nakhll/setup.py
@@ -10,6 +10,7 @@ def seutp():
     """
     override_entrylog_message_constructor()
     override_admin_panel_defaults()
+    change_default_cipher_to_seclevel_1()
 
 
 def override_entrylog_message_constructor():
@@ -25,3 +26,15 @@ def override_admin_panel_defaults():
     admin.site.site_header = "بازار آنلاین نخل"
     admin.site.site_title = "بازار آنلاین نخل"
     admin.site.index_title = "بازار آنلاین نخل، خوش آمدید!"
+
+
+def change_default_cipher_to_seclevel_1():
+    """Change default cipher to seclevel 1
+
+    To connect to SEP IPG, we need the older cipher that is seclevel 1.
+    Ubuntu 20.04 won't allow clients to connect to server with small DH key.
+    Workaround from SO: https://stackoverflow.com/a/41041028/14272982
+    """
+    import requests
+    import urllib3
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
